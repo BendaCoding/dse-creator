72783830ea7eadb0e63fa8bebab9b0bf740430ec
Jan Rahtkens <Jan@stsstudios.de>
2019-08-15 10:56:46 +0200

    Store: Add comment to custom middleware

--- src/store/persistStore.js
+++ src/store/persistStore.js
@@ -1,9 +1,10 @@
 import { IPC_EVENTS } from '../utils/enums';
 
+/**
+ * Custom middleware to persist the store upon changes
+ */
 export const persistStoreMiddleware = store => next => action => {
   next(action);
-
   const state = store.getState();
-  console.log(state);
   window.ipc.send(IPC_EVENTS.SAVE_STATE_TO_FILE, state);
 };
